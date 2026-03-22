8633ad83e3f8fbccdb1c1a457ada8979a63622e1
Jan Musial <s28977@pjwstk.edu.pl>
2026-03-22 09:19:10 +0100

    Added models described in 'Domain model - minimum scope'

---
+++ Models/Rental.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD_TASK2.Models;
+
+public class Rental
+{
+    public DateTime RentalDate { get; private set; }
+    public DateTime DueDate { get; private set; }
+    public DateTime ReturnDate { get; private set; }
+}
